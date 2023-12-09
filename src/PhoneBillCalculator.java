8a66f885a523378599723c3a942b2695996b27e4
JagodaBodnar <jagodabodnar90@gmail.com>
2023-12-09 01:53:23 +0100

    Phone bill calculator scanner close

--- src/PhoneBillCalculator.java
+++ src/PhoneBillCalculator.java
@@ -8,6 +8,7 @@ public class PhoneBillCalculator {
     public static void main(String[] args) {
         double planFee = getPlanFee();
         double overageMinutes = getOverageMinutes();
+        scanner.close();
 
         double overage = calculateOverageFees(overageMinutes);
         double tax = calculateTax(planFee, overage);
@@ -15,26 +16,30 @@ public class PhoneBillCalculator {
         printItemizedBill(planFee, overage, tax, totalFee);
 
     }
-    public static double getPlanFee(){
+
+    public static double getPlanFee() {
         System.out.println("Enter plan fee:");
         return scanner.nextDouble();
     }
-    public static double getOverageMinutes(){
+
+    public static double getOverageMinutes() {
         System.out.println("Enter overage minutes:");
-        return scanner.nextInt();
+        return scanner.nextDouble();
     }
-    public static double calculateOverageFees(double overageMinutes){
+
+    public static double calculateOverageFees(double overageMinutes) {
         return overageMinutes * overageMinutesFee;
     }
-    public static double calculateTax(double planFee, double overage){
+
+    public static double calculateTax(double planFee, double overage) {
         return (planFee + overage) * taxRate;
     }
 
-    public static double calculateTotalFee(double tax, double planFee, double overage){
+    public static double calculateTotalFee(double tax, double planFee, double overage) {
         return tax + planFee + overage;
     }
 
-    public static void printItemizedBill(double planFee, double overage, double tax, double total){
+    public static void printItemizedBill(double planFee, double overage, double tax, double total) {
         System.out.printf("Phone bill statement: %nPlan: $%.2f %nOverage: $%.2f %nTax: $%.2f %nTotal: $%.2f %n", planFee, overage, tax, total);
     }
 
